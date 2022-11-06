5b806b881335936743126ab0f937398908fbfda0
BilelBellili <bilel.bellili@esprit.tn>
2022-11-06 18:46:17 +0100

    add test

---
+++ src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
@@ -0,0 +1,113 @@
+package com.esprit.examen.services;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.esprit.examen.entities.CategorieFournisseur;
+import com.esprit.examen.entities.CategorieProduit;
+import com.esprit.examen.entities.Fournisseur;
+import com.esprit.examen.entities.Operateur;
+import com.esprit.examen.repositories.DetailFournisseurRepository;
+import com.esprit.examen.repositories.FournisseurRepository;
+import com.esprit.examen.repositories.OperateurRepository;
+import com.esprit.examen.repositories.ProduitRepository;
+import com.esprit.examen.repositories.SecteurActiviteRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+public class OperateurServiceImplTest {
+	
+	@Mock
+	OperateurRepository operateurRepository;
+	
+	@InjectMocks
+	OperateurServiceImpl operateurService;
+
+	Operateur o = Operateur.builder().nom("bellili").prenom("bilel").password("123456").build();
+	
+	List<Operateur> list = new ArrayList<Operateur>(){
+		{
+			add(Operateur.builder().nom("bellili").prenom("bilel").password("123456").build());
+			add(Operateur.builder().nom("hrizi").prenom("ilyess").password("azerty").build());
+		}
+		
+	};
+	
+	
+	@Test
+	public void testRetrieveOperateur() throws ParseException
+	{
+		Mockito.when(operateurRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(o));
+		Operateur operateur = operateurService.retrieveOperateur((long)2);
+		assertNotNull(operateur);
+		log.info("get ==>",operateur.toString());
+	}
+	
+	
+	@Test
+	public void testAddOperateur() throws ParseException 
+	{
+		Operateur tempO = new Operateur("bellili","bilel", "123456");
+		operateurService.addOperateur(tempO);
+		verify(operateurRepository, times(1)).save(tempO);
+	}
+	
+	@Test
+	public void testRetrieveAllOperateurs() throws ParseException 
+	{
+		Mockito.when(operateurRepository.findAll()).thenReturn(list);
+		List<Operateur> fourList = operateurService.retrieveAllOperateurs();
+		assertEquals(2, fourList.size());
+		verify(operateurRepository, times(1)).findAll();
+
+	}
+	/*
+	@Test
+	public void testDeleteClient() throws ParseException {
+		Fournisseur tempF = new Fournisseur("F003","Autres");
+		fournisseurService.addFournisseur(tempF);
+		fournisseurService.deleteFournisseur(tempF.getIdFournisseur());
+		verify(fournisseurRepository, times(1)).delete(tempF);
+	}
+
+	
+	
+	@Test
+	public void testUpdateCategorieProduit() throws ParseException
+	{
+		CategorieProduit c = new CategorieProduit("C001", "Alimentaire");
+		CategorieProduit categorie = categorieProduitService.addCategorieProduit(c);
+		CategorieProduit c1 = new CategorieProduit(categorie.getIdCategorieProduit(),"C002", "Alimentaire");
+		CategorieProduit categorie1 = categorieProduitService.updateCategorieProduit(c1);
+		assertEquals(categorie.getIdCategorieProduit(),categorie1.getIdCategorieProduit());
+		assertNotEquals(categorie1.getCodeCategorie(), categorie.getCodeCategorie());
+		categorieProduitService.deleteCategorieProduit(categorie.getIdCategorieProduit());
+		categorieProduitService.deleteCategorieProduit(categorie1.getIdCategorieProduit());
+	}
+	
+	*/
+	
+
+}
