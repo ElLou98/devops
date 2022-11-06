6fd1f5b0d27e8ebcd211d6879cfd5ea230a834cf
Mohamed habib Soula <79767002+medhabyb@users.noreply.github.com>
2022-11-06 16:10:17 +0100

    Update ISecteurActiviteServiceTest.java

--- src/test/java/com/esprit/examen/services/ISecteurActiviteServiceTest.java
+++ src/test/java/com/esprit/examen/services/ISecteurActiviteServiceTest.java
@@ -1,7 +1,11 @@
 package com.esprit.examen.services;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
+import java.text.ParseException;
 import java.util.List;
 
 import org.junit.Test;
@@ -17,28 +21,55 @@ public class ISecteurActiviteServiceTest extends Tests {
 	ISecteurActiviteService ss;
 	
 	@Test
-	@Order(1)
 	public void testaddSecteurActivite()
 	{
-		SecteurActivite s = new SecteurActivite("10","tunis");
-		SecteurActivite saveds = ss.addSecteurActivite(s);
-		assertEquals(s.getlibelleSecteurActivite(),saveds.getlibelleSecteurActivite());
+		SecteurActivite sa = new SecteurActivite("10","tunis");
+		SecteurActivite sadb = ss.addSecteurActivite(sa);
+		assertNotNull(sadb.getidSecteurActivite());
+		assertNotNull(sadb.getlibelleSecteurActivite());
+		ss.deleteSecteurActivite(sadb.getidSecteurActivite());
 	}
 	
 	@Test
-	@Order(2)
 	public void testretrieveAllSecteurActivite()
 	{
 		List<SecteurActivite> allStocks = ss.retrieveAllSecteurActivite();
-		assertEquals(4,allStocks.size());
+		int expected = allStocks.size();
+		SecteurActivite sa = new SecteurActivite("10","tunis");
+		SecteurActivite sadb = ss.addSecteurActivite(sa);
+		assertEquals(expected,allStocks.size());
+		ss.deleteSecteurActivite(sadb.getidSecteurActivite());
 	}
 	
 	@Test
-	@Order(3)
 	public void testretrieveSecteurActivite()
-	{
-		SecteurActivite saveds = ss.retrieveSecteurActivite(1L);
-		assertEquals(1L,saveds.getidSecteurActivite().longValue());
+	{   SecteurActivite sa = new SecteurActivite("10","tunis");
+	    SecteurActivite sadb = ss.addSecteurActivite(sa);
+		SecteurActivite sadb1 = ss.retrieveSecteurActivite(sadb.getidSecteurActivite());
+		assertNotNull(sadb1);
+		ss.deleteSecteurActivite(sadb.getidSecteurActivite());
 	}
+	
+	@Test
+	public void testUpdateCategorieProduit() throws ParseException
+	{
+		SecteurActivite sa = new SecteurActivite("10","tunis");
+		SecteurActivite sadb = ss.addSecteurActivite(sa);
+		SecteurActivite sa1 = new SecteurActivite(sadb.getidSecteurActivite(),"20", "gabes");
+		SecteurActivite sadb1 = ss.updateSecteurActivite(sa1);
+		assertEquals(sa.getidSecteurActivite(),sa1.getidSecteurActivite());
+		assertNotEquals(sadb.getlibelleSecteurActivite(), sadb1.getlibelleSecteurActivite());
+		ss.deleteSecteurActivite(sadb.getidSecteurActivite());
+	}
+	
+	@Test
+	public void testdeleteSecteurActivite()
+	{   SecteurActivite sa = new SecteurActivite("10","tunis");
+	    SecteurActivite sadb = ss.addSecteurActivite(sa);
+		ss.deleteSecteurActivite(sadb.getidSecteurActivite());
+		assertNull(ss.retrieveSecteurActivite(sadb.getidSecteurActivite()));
+	}
+	
+	
 
 }
